56385e4436807c0d719c09f620c279a514983413
KumarHarsh2001 <60170479+KumarHarsh2001@users.noreply.github.com>
2021-02-03 21:51:09 +0530

    Source Code
    Program of advance traffic control system

---
+++ skeSENSOR.ino
@@ -0,0 +1,25 @@
+const int l=P1_6;
+const int s=P1_0;
+const int t=200;
+void setup() {
+Serial.begin(9600);
+pinMode(l,OUTPUT);
+pinMode(s,INPUT);
+}
+
+void loop() {
+
+int s1=analogRead(s);
+if(s1>=t)
+{
+  digitalWrite(l,HIGH);
+delay(30000);
+
+
+}
+else
+{
+digitalWrite(l,LOW);
+  
+  }
+}
